fbad546805bc75b09cbe385c9a568b230aaa8ed1
flowirtz <flowirtz@users.noreply.github.com>
2020-07-30 16:32:17 +0000

    Merge pull request #66 from openclimatefix/add-more-logos openclimatefix/website@9aa842d83c67aa4b71bf820617692add86d88e7f

---
+++ 252f366e-13548e38a65ffca02a30.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{NIcq:function(t,a,c){"use strict";c.d(a,"a",(function(){return i}));var n=c("Lnxd"),i=function(t){return Object(n.a)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"}}]})(t)};i.displayName="MdEmail"}}]);
+//# sourceMappingURL=252f366e-13548e38a65ffca02a30.js.map
